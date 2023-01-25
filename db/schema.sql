a37d84c3af0cd20a2dd8e1637e8208474a1e8262
maiaferrer <mferrer2@uncc.edu>
2023-01-25 14:30:37 -0500

    added databases to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,25 @@
 -- Drop Tables Employee, Roles, Department
-
-
--- Create Employee Table
-
--- Create Roles Table
+DROP TABLE IF EXISTS deparment_db;
+DROP TABLE IF EXISTS role_db;
+DROP TABLE IF EXISTS employee_db;
 
 -- Create Department Table
+CREATE TABLE department_db (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+-- Create Roles Table
+CREATE TABLE role_db (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+);
+-- Create Employees Table
+CREATE TABLE employee_db (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER REFERENCES employee_db,
+);
